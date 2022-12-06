d043b70c6ed2335723fe5737c8ba05e6cbd5c58d
Halit OTURAK <halithamza0920@gmail.com>
2022-12-06 12:54:49 +0300

    FeatureE and main created

--- src/test/java/FeatureE.java
+++ src/test/java/FeatureE.java
@@ -1,7 +1,12 @@
 public class FeatureE {
     public static void main(String[] args) {
+<<<<<<< HEAD
         
     }
 
     // didn't merge yet feature-e
+=======
+
+    }
+>>>>>>> b412aa3 (FeatureE and main created)
 }
